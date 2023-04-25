7730394858c90e50af3e96fbbd1e9af6abe2e0c3
re-gis <irumvaregisdmc@gmail.com>
2023-04-25 20:51:08 +0200

    Making a searching screen

--- frontend/src/screens/MessagesScreen.jsx
+++ frontend/src/screens/MessagesScreen.jsx
@@ -4,6 +4,14 @@ import Menu from "../components/Menu";
 import { Store } from "../Store";
 import MessageUsers from "../components/messages/MessageUsers";
 import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const StyledButton = styled(Button)({
+  width: '100px',
+  height: '23px', 
+  marginTop: '10px'
+})
+
 
 const MessagesScreen = () => {
   const { state, dispatch } = useContext(Store);
@@ -35,7 +43,7 @@ const MessagesScreen = () => {
               <div style={{ flex: "0.35" }} className="border-r flex flex-col">
                 <div style={{ flex: 0.1 }} className="border-b">
                   <div className="flex items-center justify-between">
-                    <div className="flex items-center gap-2 pl-24">
+                    <div className="flex items-center gap-2 pl-24 cursor-pointer">
                       <div className="py-5">
                         <h1 style={{ fontWeight: 500, fontSize: "10.5px" }}>
                           {userInfo.username}
@@ -58,7 +66,7 @@ const MessagesScreen = () => {
                         </svg>
                       </div>
                     </div>
-                    <div className="pr-6">
+                    <div className="pr-6 cursor-pointer">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -83,33 +91,63 @@ const MessagesScreen = () => {
                   <MessageUsers />
                 </div>
               </div>
-              <div style={{ flex: "0.65" }}>
-                <div>
-                  <div className="w-32 rounded-full h-32 bg-red-50 flex">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-6 h-6"
+              <div
+                style={{
+                  flex: "0.65",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignSelf: "auto",
+                  alignItems: "stretch",
+                  justifyContent: "flex-start",
+                  position: "relative",
+                  flexGrow: 1,
+                }}
+              >
+                <div
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    padding: "24px",
+                    justifyContent: "center",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    flexShrink: 0,
+                    alignSelf: "auto",
+                    position: "relative",
+                    flexGrow: 0,
+                    gap: "5px",
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="0.5"
+                    stroke="currentColor"
+                    class="w-6 h-6"
+                    className="border-black border-2 rounded-full w-24 h-24 p-3"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                    />
+                  </svg>
+                  <Typography variant="span" style={{ fontWeight: 500 }}>
+                    Your Messages
+                  </Typography>
+                  <Typography variant="span" style={{ fontSize: '9px' }} className="text-gray-500">
+                    Send private photos and messages to a friend or group.
+                  </Typography>
+                  <StyledButton variant="contained" size="small">
+                    <span
+                      className="text-xs"
+                      style={{ textTransform: "capitalize" }}
                     >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                      />
-                    </svg>
-                  </div>
-                  <div>
-                    <Typography variant="span">Your Messages</Typography>
-                    <Typography variant="span">
-                      Send private photos and messages to a friend or group.
-                    </Typography>
-                    <Button variant="contained" size="small">
                       Send Message
-                    </Button>
-                  </div>
+                    </span>
+                  </StyledButton>
                 </div>
               </div>
             </div>
